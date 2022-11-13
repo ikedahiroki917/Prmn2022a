e217a5ce017d0976e6e4ced58587b245dbb95123
ikedahiroki917 <b2210120@photon.chitose.ac.jp>
2022-11-13 13:18:59 +0900

    lecture02-2を提出

---
+++ prmn2022a/prmn2022a/src/lecture02/Car.java
@@ -0,0 +1,16 @@
+package lecture02;
+
+public class Car {
+    int fuel;
+    Car(){
+        this.fuel = 0;
+    }
+    void run(){
+        if(this.fuel > 0){
+            this.fuel = this.fuel - 1;
+            System.out.println("燃料を1消費して走りました。");
+        }else{
+            System.out.println("燃料がありません。");
+        }
+    }
+}
